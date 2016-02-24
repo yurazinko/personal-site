e6827e90c70db3a99a8b8bc067540a051613997a
yurazinko <yurazinko@ukr.net>
2016-02-24 21:30:16 +0000

    jquery

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-// Navigation menu on top:
-$(function(f){
+// Navigation menu on top
+$(function(f) {
     var element = f('.navbar');
-    f(window).scroll(function(){
-        element['fade'+ (f(this).scrollTop() > 400 ? 'In': 'Out')](300);           
+    f(window).scroll(function() {
+        element['fade' + (f(this).scrollTop() > 400 ? 'In' : 'Out')](300);
     });
 });
 
-// Scrolling to elements:
-$(document).ready(function(){
-    $('a[href^="#"], a[href^="."]').click( function(){ // если в href начинается с # или ., то ловим клик
-	    var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href
+// Scrolling to elements
+$(document).ready(function() {
+    $('a[href^="#"], a[href^="."]').click(function() { // если в href начинается с # или ., то ловим клик
+        var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href
         if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+            $('html, body').animate({
+                scrollTop: $(scroll_el).offset().top
+            }, 500); // анимируем скроолинг к элементу scroll_el
         }
-	    return false; // выключаем стандартное действие
+        return false; // выключаем стандартное действие
     });
 });
 
+
+$('.toggle').click(function (event) {
+	event.preventDefault();
+	var target = $(this).attr('href');
+	$(target).toggleClass('hidden show');
+});
