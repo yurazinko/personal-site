afda1f8da1c2e8950e0f6c849c00cbb7661f0b4d
yurazinko <yurazinko@ukr.net>
2016-02-24 09:34:01 +0000

    jquery

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+// Navigation menu on top:
 $(function(f){
     var element = f('.navbar');
     f(window).scroll(function(){
         element['fade'+ (f(this).scrollTop() > 400 ? 'In': 'Out')](300);           
+    });
+});
+
+// Scrolling to elements:
+$(document).ready(function(){
+    $('a[href^="#"], a[href^="."]').click( function(){ // если в href начинается с # или ., то ловим клик
+	    var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+        }
+	    return false; // выключаем стандартное действие
     });
 });
 
