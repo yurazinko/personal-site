c3deb86c7ed10e7617bc4d982efe330e2e597f60
yurazinko <yurazinko@ukr.net>
2016-02-26 12:15:55 +0000

    last css

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ $(function(f) {
 
 // Scrolling to elements
 $(document).ready(function() {
-    $('a[href^="#"], a[href^="."]').click(function() { // если в href начинается с # или ., то ловим клик
-        var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href
-        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+    $('a[href^="#"], a[href^="."]').click(function() { 
+        var scroll_el = $(this).attr('href'); 
+        if ($(scroll_el).length != 0) { 
             $('html, body').animate({
                 scrollTop: $(scroll_el).offset().top
-            }, 500); // анимируем скроолинг к элементу scroll_el
+            }, 500); 
         }
-        return false; // выключаем стандартное действие
+        return false; 
     });
 });
 
